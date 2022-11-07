4c62d407449832dd0a2f6c212563337aebdb40f0
aboumil <aboumil@a208pc07L.local.isima.fr>
2022-11-07 15:20:46 +0100

    mvmts

--- tp4/test/test_mouchard.cpp
+++ tp4/test/test_mouchard.cpp
@@ -4,33 +4,39 @@
 
 // Fonction principale //---------------------------------------------------------------------------
 int main(void) {
- complexe_t::stats();
+ complexe_t::stats(); //0 0 0 0 
 
  std::cout << std::endl << "complexe_t c1(1,2);" << std::endl;
- complexe_t c1(1,2);
- complexe_t::stats();
+ complexe_t c1(1,2); 
+ complexe_t::stats(); //0 1 0 0 construction simple
 
  std::cout << std::endl << "complexe_t c2(3,4);" << std::endl;
  complexe_t c2(3,4);
- complexe_t::stats();
+ complexe_t::stats(); //0 2 0 0 construction simple
 
  std::cout << std::endl << "complexe_t c3(c1);" << std::endl;
- complexe_t c3(c1);
- complexe_t::stats();
+ complexe_t c3(c1); //construction par copie -> car c1 lvalue
+ complexe_t::stats(); //0 3 1 0 copie
 
  std::cout << std::endl << "complexe_t c4(std::move(c2));" << std::endl;
- complexe_t c4(std::move(c2));
- complexe_t::stats();
+ complexe_t c4(std::move(c2)); //construction par mouvement, converts c2 to rvalue
+ complexe_t::stats(); //0 4 1 1
 
  std::cout << std::endl << "c3=c1;" << std::endl;
- c3=c1;
- complexe_t::stats();
+ c3=c1; //affectation + copie
+ complexe_t::stats(); //1 4 2 1
 
  std::cout << std::endl << "c4=complexe_t(5,6);" << std::endl;
- c4=complexe_t(5,6);
- complexe_t::stats();
+ c4=complexe_t(5,6); //rvalue en params -> affectation + copie mouvement
+ complexe_t::stats(); //2 5 2 2
 
  return 0;
 }
+
+//affec  cpy 1
+//affec  mvm 1
+//constr spl 3
+//constr cpy 1
+//constr mvm 1
 
 // Fin //-------------------------------------------------------------------------------------------
--- tp4/test/test_vecteur.cpp
+++ tp4/test/test_vecteur.cpp
@@ -12,47 +12,61 @@ int main(void) {
 
  std::cout << std::endl << "v1 = " << v1 << std::endl;
  std::cout << "v2 = " << v2 << std::endl;
- complexe_t::stats();
+ //constructions par defaut 20
+ complexe_t::stats();  //0 20 0 0
 
+ //20 rvalues -> 20 affct par mouvement 
+ //20 construction ad hoc
  for (unsigned i = 0; i<v1.getTaille(); ++i) v1[i]=complexe_t(10*i,i*i);
  for (unsigned i = 0; i<v2.getTaille(); ++i) v2[i]=complexe_t(i,i+1);
 
  std::cout << std::endl << "v1 = " << v1 << std::endl;
  std::cout << "v2 = " << v2 << std::endl;
- complexe_t::stats();
+ complexe_t::stats(); //20 40 0 20
 
- Vecteur v3 = v1;
 
+ //10 construction par def + 10 affectations par copie
+ Vecteur v3 = v1; 
  std::cout << std::endl << "v3 = " << v3 << std::endl;
- complexe_t::stats();
+ complexe_t::stats(); //30 50 10 20
 
- Vecteur v4;
 
- v4=v1+v2;
+ Vecteur v4; //10 par déf constructions
+ v4 = v1+v2; //10 affec copie
+ /*
+  {
+      10 par déf
+
+      10 affect mvm (+ des complexes)
+      10 adhoc
+
+      10 construction mvm ?
+  }
+ */
 
  std::cout << std::endl << "v4 = " << v4 << std::endl;
- complexe_t::stats();
+ complexe_t::stats();  //50 80 20 30
 
- Vecteur v5;
+//  Vecteur v5;
 
- v5=v1*(v2+v4);
+//  v5=v1*(v2+v4);
 
- std::cout << std::endl << "v5 = " << v5 << std::endl;
- complexe_t::stats();
+//  std::cout << std::endl << "v5 = " << v5 << std::endl;
+//  complexe_t::stats();
 
- std::deque<complexe_t> liste;
+//  std::deque<complexe_t> liste;
 
- for (unsigned i = 0; i<v5.getTaille(); ++i) liste.push_back(v5[i]);
+//  for (unsigned i = 0; i<v5.getTaille(); ++i) liste.push_back(v5[i]);
 
- std::cout << std::endl << "liste = v5" << std::endl;
- complexe_t::stats();
+//  std::cout << std::endl << "liste = v5" << std::endl;
+//  complexe_t::stats();
 
- std::sort(liste.begin(),liste.end(),ComparateurComplexe());
+//  std::sort(liste.begin(),liste.end(),ComparateurComplexe());
 
- std::cout << std::endl;
- for (unsigned i = 0; i<liste.size(); ++i) std::cout << liste[i] << " ";
- std::cout << std::endl;
- complexe_t::stats();
+//  std::cout << std::endl;
+//  for (unsigned i = 0; i<liste.size(); ++i) std::cout << liste[i] << " ";
+//  std::cout << std::endl;
+//  complexe_t::stats();
 }
 
 // Fin //-------------------------------------------------------------------------------------------
